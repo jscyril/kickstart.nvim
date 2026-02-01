2b4f58c20ace98e8212de5b6c0dd0b35684ee6e4
jscyril <jacobsebcyril@gmail.com>
2026-02-01 15:07:14 +0530

    added transparency

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,5 +1,23 @@
 -- Core Autocommands
 
+-- Apply transparency (remove background so terminal transparency shows through)
+vim.api.nvim_create_autocmd('ColorScheme', {
+  desc = 'Apply transparent background after colorscheme loads',
+  group = vim.api.nvim_create_augroup('user-transparency', { clear = true }),
+  callback = function()
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE', ctermbg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'NONE', ctermbg = 'NONE' })
+  end,
+})
+
+-- Apply transparency immediately for current session
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'NONE', ctermbg = 'NONE' })
+
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
